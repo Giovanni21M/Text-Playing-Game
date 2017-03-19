82147a55361dca4bd71168f3cc95d4574a83a03e
Giovanni21M <giovanni21martinus@gmail.com>
2017-03-19 19:15:30 -0400

    Simplify game

--- base.py
+++ base.py
@@ -36,12 +36,6 @@ class Beginning(Scene):
         return 'guildhall'
 
 
-class Caves(Scene):
-
-    def enter(self):
-        pass
-
-
 class Death(Scene):
 
     quips = [
@@ -54,12 +48,6 @@ class Death(Scene):
     def enter(self):
         print(Death.quips[randint(0, len(self.quips)-1)])
         exit(1)
-
-
-class Demon(Scene):
-
-    def enter(self):
-        pass
 
 
 class Dragon(Scene):
@@ -76,18 +64,6 @@ class Finished(Scene):
         return 'finished'
 
 
-class Forest(Scene):
-
-    def enter(self):
-        pass
-
-
-class Golem(Scene):
-
-    def enter(self):
-        pass
-
-
 class GuildHall(Scene):
 
     def enter(self):
@@ -97,36 +73,28 @@ class GuildHall(Scene):
         print("2. Visit the blacksmith.")
         print("3. Visit the armorsmith.")
         
-        while True:
-            response = input("Well...? ")
-            response = response.lower()
+        choice = input("Well...? ")
+        choice = choice.lower()
 
-            if response == "1" or "quest" or "go on quest":
-                print("Where does your adventure take you?")
-                print("Maze")
-                print("Mountain")
-                print("Forest")
-                print("Caves")
+        if choice == "1" or "quest" or "go on quest":
+            print("Where does your adventure take you?")
+            print("Maze")
+            print("Mountain")
                 
-                while True:
-                    zone = input("")
-                    zone = zone.lower()
-                    if zone == "maze":
-                        return 'maze'
-                    elif zone == 'mountain':
-                        return 'mountain'
-                    elif zone == 'forest':
-                        return 'forest'
-                    elif zone == 'caves' or 'caves':
-                        return 'caves'
-                    else:
-                        print("There are beasts to be slain, hurry and choose!")
-            elif response == "2" or "blacksmith" or "visit the blacksmith":
-                pass
-            elif response == "3" or "armorsmith" or "visit the armorsmith":
-                pass
+            choice2 = input("")
+            choice2 = choice2.lower()
+            if choice2 == "maze":
+                return 'maze'
+            elif choice2 == 'mountain':
+                return 'mountain'
             else:
-                print("That's not an option.")
+                print("There are beasts to be slain, hurry and choose!")
+        elif choice == "2" or "blacksmith" or "visit the blacksmith":
+            pass
+        elif choice == "3" or "armorsmith" or "visit the armorsmith":
+            pass
+        else:
+            print("That's not an option.")
 
 
 class Maze(Scene):
@@ -135,53 +103,51 @@ class Maze(Scene):
         print("You enter Pandora's Labyrinth")
         print("and hear a light roar coming from inside.")
         
-        while True:
-            choice = input("Do you go turn back or go deeper? ")
-            choice = choice.lower()
+        choice = input("Do you go turn back or go deeper? ")
+        choice = choice.lower()
 
-            if choice == "back" or "turn back" or "go back":
-                return 'guildhall'
-            elif choice == "deeper" or "go deeper":
-                while True:
-                    trip_choice = input(
-                        """
-                        There are now three directions
-                        to choose from. Do you go left,
-                        right, or head forward? 
-                        """
-                    )
-                    trip_choice = trip_choice.lower()
-                    if trip_choice == "left" or "go left":
-                        print("You've decided to go down the left path.")
-                        print("You head deeper and deeper,")
-                        print("the cries are getting louder and louder.")
-                        while True:
-                            choice_boss = input(
-                                """
-                                Head towards the loud roar
-                                or towards the light? 
-                                """
-                            )
-                            choice_boss == choice_boss.lower()
-                            if choice_boss == "light":
-                                return 'forest'
-                            elif choice_boss == "roar" or "loud roar":
-                                return 'minotaur'
-                            else:
-                                "MAKE YOUR CHOICE BEFORE IT'S TOO LATE!"
-                    elif trip_choice == "right" or "go right":
-                        print("You've decided to make the right choice")
-                        print("and see a light up ahead. You walk into it.")
-                        return 'maze'
-                    elif trip_choice == "straight" or "go straight":
-                        print("You're heading deeper and deeper.")
-                        print("It's now getting hotter as well.")
-                        print("You've fallen through a hole in the floor.")
-                        return 'caves'
-                    else:
-                        print("Make your choice, dying soul.")
-            else:
-                print("Are you so scared of the unknown?")
+        if choice == "back" or "turn back" or "go back":
+            return 'guildhall'
+        elif choice == "deeper" or "go deeper":
+            print("Deeper we're going young padawan.")
+           # trip_choice = input(
+           #     """
+           #     There are now three directions
+           #     to choose from. Do you go left,
+           #     right, or head forward? 
+           #     """
+           # )
+           # trip_choice = trip_choice.lower()
+           # if trip_choice == "left" or "go left":
+           #     print("You've decided to go down the left path.")
+           #     print("You head deeper and deeper,")
+           #     print("the cries are getting louder and louder.")
+           #     choice_boss = input(
+           #         """
+           #         Head towards the loud roar
+           #         or towards the light? 
+           #         """
+           #     )
+           #     choice_boss == choice_boss.lower()
+           #     if choice_boss == "light":
+           #         return 'forest'
+           #     elif choice_boss == "roar" or "loud roar":
+           #         return 'minotaur'
+           #     else:
+           #         "MAKE YOUR CHOICE BEFORE IT'S TOO LATE!"
+           # elif trip_choice == "right" or "go right":
+           #     print("You've decided to make the right choice")
+           #     print("and see a light up ahead. You walk into it.")
+           #     return 'maze'
+           # elif trip_choice == "straight" or "go straight":
+           #     print("You're heading deeper and deeper.")
+           #     print("It's now getting hotter as well.")
+           #     print("You've fallen through a hole in the floor.")
+           #     return 'caves'
+           # else:
+           #     print("Make your choice, dying soul.")
+        else:
+            print("Are you so scared of the unknown?")
 
 
 class Minotaur(Scene):
@@ -196,29 +162,17 @@ class Mountain(Scene):
         pass
 
 
-class Wyvern(Scene):
-
-    def enter(self):
-        pass
-
-
-
 class Map(object):
 
     scenes = {
         'beginning': Beginning(),
-        'caves': Caves(),
         'death': Death(),
-        'demon': Demon(),
         'dragon': Dragon(),
         'finished': Finished(),
-        'forest': Forest(),
-        'golem': Golem(),
         'guildhall': GuildHall(),
         'maze': Maze(),
         'minotaur': Minotaur(),
         'mountain': Mountain(),
-        'wyvern': Wyvern(),
     }
 
     def __init__(self, start_scene):
