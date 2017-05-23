09f429e76a7b2cd49ea66b70d314bb4510971a5f
Giovanni21M <giovanni21martinus@gmail.com>
2017-05-22 21:37:31 -0400

    Set GUI title and size

--- gui.py
+++ gui.py
@@ -6,7 +6,9 @@ from gi.repository import Gtk
 class MainWindow(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="")
+        Gtk.Window.__init__(self, title="Text Playing Game")
+        self.set_border_width(10)
+        self.set_size_request(500, 400)
 
 
 win = MainWindow()
